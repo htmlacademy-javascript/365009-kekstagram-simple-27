2ee181c5e260ad9f3573cba781d150bf58996b14
Denis <kaliff.90@yandex.ru>
2022-10-08 22:32:02 +0400

    Правки в функции рандомного числа

--- js/main.js
+++ js/main.js
@@ -1,19 +1,37 @@
-//Функция, возвращающая случайное целое число из переданного диапазона включительно
+// Функция, возвращающая случайное целое число из переданного диапазона включительно
 function getRandomPositiveNumber (min, max) {
+  // min или max не являются числом
+  if (typeof min !== 'number' || typeof max !== 'number') {
+    return NaN;
+  }
+  // min или max равно бесконечности
+  if (!Number.isFinite(min) || !Number.isFinite(max)) {
+    return NaN;
+  }
+  // min или max отрицательное
   if (min < 0 || max < 0) {
-    throw 'Недопустимое число';
+    return NaN;
   }
-  if (min > max) {
-    max = min + (min = max) - max;
+  // Нет целых чисел в диапазоне
+  if (Math.abs(max - min) < 1) {
+    return NaN;
   }
-  const lower = Math.ceil(Math.min(min, max));
-  const upper = Math.floor(Math.max(min, max));
-  const result = Math.random() * (upper - lower + 1) - lower;
+
+  const safeMin = Math.min(min, max);
+  const safeMax = Math.max(min, max);
+
+  const lower = Math.ceil(safeMin);
+  const upper = Math.floor(safeMax);
+
+  const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 }
 
-//Функция для проверки максимальной длины строки
-const getTextLength = (string, maxLength) => string.length > maxLength
+// Функция для проверки максимальной длины строки
+
+function isTextLength (string, maxLength) {
+  return string.length > maxLength;
+}
 
 getRandomPositiveNumber(1, 4);
-getTextLength('keks', 9);
+isTextLength('keks', 9);
